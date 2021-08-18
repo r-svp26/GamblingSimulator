c3cdc4f7ac224f3f03cb8e918813ce724d0caf92
r-svp26 <riteshk6341@gmail.com>
2021-08-18 10:23:14 +0530

    [Ritesh] Add: Player win or loss the game

--- GamblerSimulator/Player.cs
+++ GamblerSimulator/Player.cs
@@ -9,5 +9,18 @@ namespace GamblerSimulator
         // constants
         const int STAKE_PER_DAY = 100;
         const int BET_PER_GAME = 1;
+        const int WIN = 1;
+        /// <summary>
+        /// check the player game status
+        /// </summary>
+        public static void GameStatus()
+        {
+            Random random = new Random();
+            int stake = random.Next(1, 3);
+            if (stake == WIN)
+                Console.WriteLine("Playe win the game");
+            else
+                Console.WriteLine("Playe loss the game");
+        }
     }
 }
--- GamblerSimulator/Program.cs
+++ GamblerSimulator/Program.cs
@@ -4,9 +4,13 @@ namespace GamblerSimulator
 {
     class Program
     {
+        /// <summary>
+        ///  main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Gambler Simulator");
+            Player.GameStatus();
         }
     }
 }
