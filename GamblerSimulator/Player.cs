1549b0f9029e038b6c379c43f867e5bdfa25810c
r-svp26 <riteshk6341@gmail.com>
2021-08-18 09:28:40 +0530

    [Ritesh] Add: Game started with stake

---
+++ GamblerSimulator/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblerSimulator
+{
+    public class Player
+    {
+        // constants
+        const int STAKE_PER_DAY = 100;
+        const int BET_PER_GAME = 1;
+    }
+}
