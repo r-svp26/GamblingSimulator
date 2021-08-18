903092f2e056f0241c73d65150f5dfa97fffa723
r-svp26 <riteshk6341@gmail.com>
2021-08-18 09:20:52 +0530

    [Ritesh] Add: Display welcome message

---
+++ GamblerSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GamblerSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
